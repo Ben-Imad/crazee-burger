bfcc6a8a8afc28804d4cc3c6241f68a4880ae7c1
Ben-Imad KOUAKAN <benimad.ci@yeswecange.com>
2024-06-22 17:26:24 +0000

    🔥 F01: Delete App.test.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Salut Ben-Imad.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // state (état, données)
+  const [prenom, setPrenom] = useState("");
+
+
+  // comportements
+  const handlePrenomChange = (event) => {
+
+  }
+  const handleSubmit = (event) => {
+
+  }
+
+  // affichage (render)
+  return <>
+
+  </>
 }
 
-export default App;
+export default App
