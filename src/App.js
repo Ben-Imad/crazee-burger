219e03e48c09201f8b57c77333497adab0b01aed
Ben-Imad KOUAKAN <benimad.ci@yeswecange.com>
2024-06-26 03:31:32 +0000

    ✨ F02: APP > Install routing system

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import LoginPage from "./components/pages/login/LoginPage"
 import OrderPage from "./components/pages/order/OrderPage"
-import ErrorPage from "./components/pages/ErrorPage"
-import Layout from "./components/pages/Layout"
+import ErrorPage from "./components/pages/error/ErrorPage"
 
 function App() {
   return <>
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<Layout />} /> */}
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/order/:name" element={<OrderPage />} />
-        <Route path="/pageQuiNExistePas" element={<ErrorPage/>}/>
-      </Routes>
-    </BrowserRouter>
-    {/* <LoginPage /> */}
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/order/:name" element={<OrderPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   </>
 
 }
